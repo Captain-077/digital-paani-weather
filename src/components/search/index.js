89d977d5fa0f22b485f86f14ac8ac1389ca85175
Your Name <nakuldesigner77@gmail.com>
2024-04-26 22:00:28 +0530

    changes compelted

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 
 function Search({search,setSearch,handleSearch}) {
 
-    // const[search,setSearch] = useState("")
-
   return (
     <div className="search-engine">
         <input type="text" placeholder='Enter city name' name='search' value={search} onChange={(event) => setSearch(event.target.value)}/>
